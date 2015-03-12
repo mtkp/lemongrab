61866850034ee551b8227ea7759ca8f09a088ebe
Matt Kipps <matkips@gmail.com>
2015-03-11 20:50:14 -0700

    update api examples for new routes

--- api-examples.sh
+++ api-examples.sh
@@ -4,23 +4,23 @@ url=http://localhost:5000
 
 echo "-- get app --"
 curl -i $url
-echo "\n"
+echo ; echo
 
 echo "-- browser tries to get list --"
 curl -i \
   $url/list/get-a-list
-echo "\n"
+echo ; echo
 
 echo "-- app tries to get list --"
 curl -i \
-  -H "Content-Type: application/json" \
-  $url/list/get-a-list
-echo "\n"
+  -H "Accept: application/json" \
+  $url/api/v1/list/get-a-list
+echo ; echo
 
 echo "-- app posts to list --"
 curl -i \
-  -X POST \
+  -X PUT \
   -H "Content-Type: application/json" \
-  -d '{"items":[]}' \
-  $url/list/post-to-a-list
-echo ""
+  -d '{"items":["item 1"]}' \
+  $url/api/v1/list/post-to-a-list
+echo ; echo
