cd0b93ee5e29b39614dc9fd6c3a4e2cfe9e235d5
Matt Kipps <matkips@gmail.com>
2015-03-22 17:44:58 -0700

    refactoring

--- lemongrab/utils.py
+++ lemongrab/utils.py
@@ -10,16 +10,23 @@ def jsonify_response(f):
         return json.jsonify(f(*args, **kwargs))
     return wrapped_f
 
+bad_schema_error = ('Invalid schema', 403)
+
+def is_valid(o, schema):
+    try:
+        validate(o, schema)
+    except ValidationError:
+        return False
+    return True
+
 def validate_response(schema):
     def wrapped_schema(f):
         @functools.wraps(f)
         def wrapped_f(*args, **kwargs):
             o = f(*args, **kwargs)
-            try:
-                validate(o, schema)
-            except ValidationError:
-                return 'Invalid schema', 403
-            return o
+            if is_valid(o, schema):
+                return o
+            return invalid_schema
         return wrapped_f
     return wrapped_schema
 
@@ -27,11 +34,8 @@ def validate_request(schema):
     def wrapped_schema(f):
         @functools.wraps(f)
         def wrapped_f(*args, **kwargs):
-            try:
-                validate(request.get_json(cache=True), schema)
-            except ValidationError:
-                return 'Invalid schema', 403
-            return f(*args, **kwargs)
+            if is_valid(request.get_json(cache=True), schema):
+                return f(*args, **kwargs)
+            return invalid_schema
         return wrapped_f
     return wrapped_schema
-
