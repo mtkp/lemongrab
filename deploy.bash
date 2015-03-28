13362f01499c4cf9b110c39645406e365d5e440e
Matt Kipps <matkips@gmail.com>
2015-03-28 16:18:36 -0700

    less yellow

--- deploy.bash
+++ deploy.bash
@@ -5,15 +5,19 @@ if hash eb 2>/dev/null ; then
   echo "Elasic Beanstalk CLI installed."
 else
   echo "Need to install Elastic Beanstalk CLI to deploy."
-  echo "\"$ sudo pip install awsebcli\""
+  echo "--> $ sudo pip install awsebcli"
+  # you will also need to initialize eb and app environment
+  # for lemongrab
+  # (application name is lemongrab, region is us-west-2)
+  # (create lemongrab-dev)
+  #
+  # for example:
+  # $ eb init     # init
+  # $ eb create   # create an environment
   exit 1
 fi
 
-# eb init     # init
-# eb create   # create an environment
-
 set -e
-
 # view existing envs
 eb list
 
